5c64ac19fde9a97e00d66492b1b5550204b85f6f
Николай <99831971+YaKolyash@users.noreply.github.com>
2023-04-06 18:45:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Time: 818ms Passed: 2Failed: 0
+// Test Results:
+// Basic tests
+// Testing for fixed tests
+// Completed in 1ms
+// Random tests
+// Testing for 40 random tests
+// Completed in 1ms
+// You have passed all of the tests! :)
+
+
+
+function greet(name) {
+    return `Hello, ${name} how are you doing today?`;
+}
+
+
